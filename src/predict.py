5b2715dea6f86c3c35f8485702d68d80a2c2923a
sathvik udupa <sathvikudupa66@gmail.com>
2020-05-13 10:53:34 +0530

    lstm code

--- src/predict.py
+++
@@ -1,8 +0,0 @@
-from utils import load_data
-
-
-
-if __name__ == '__main__':
-    load_data = Load_data()
-    cell_data = Load_data(name='data.mat', mat_key='data', file_path='../../NDTPhase2/data/')
-    print(cell_data.shape)
